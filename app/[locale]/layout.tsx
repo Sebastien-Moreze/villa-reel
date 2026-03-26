d716a2c1f493543d28abf4393a357ee10c516c63
Sébastien <saanesu@gmail.com>
2026-03-26 11:01:56 +0100

    Fix favicon: ajout icons dans metadata du locale layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -13,6 +13,14 @@ export const metadata: Metadata = {
   title: "Villa R.E.E.L",
   description:
     "Villa d'exception pour séjours, événements d'entreprise et collaborations créatives.",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 type Props = {
